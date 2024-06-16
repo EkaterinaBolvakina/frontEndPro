14dad98b579550432e83024044ee372904b1b023
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-06-16 22:40:58 +0200

    updated nav

--- 14_redux_RTK_13_06_2024/src/components/main/Counter.tsx
+++ 14_redux_RTK_13_06_2024/src/components/main/Counter.tsx
@@ -14,7 +14,7 @@ const Counter = () => {
    }
 
   return (
-    <div>
+    <div className='counterGeneral'>
         <div>Counter: {counter}</div>
         <button onClick={handleMinus}>Minus (Decrement)</button>
         <button onClick={handlePlus}>Plus (Increment)</button>
